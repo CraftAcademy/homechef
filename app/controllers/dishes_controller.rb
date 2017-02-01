125620e17b2f4a31808392352145ab42e5e64afa
rmzse <rodrigo@therawdelicious.com>
2017-02-01 16:15:41 +0100

    remove redundant path to view, acceptance test failing, missing field, but view is accessed

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -1,16 +1,10 @@
 class DishesController < ApplicationController
 
-
 	def show
     @dish = Dish.find(params[:id])
 	end
 
-	def my_dishes
-		visit users_dishes_path
-	end
-
 	def new
-		redirect_to new_user_dish
 	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users, path: 'dishes'
 	resources :users do
-		resources :dishes
+		resources :dishes, only: [:new, :create]
 	end
   root controller: :landing, action: :index
   resources :dishes, only: [:index, :show]
-  get 'my_dishes', to: :my_dishes, controller: 'dishes'
   resources :checkout, only: [:index]
   resources :charges, only: [:create]
   post :add_to_order, controller: :orders, action: :add_to_order
