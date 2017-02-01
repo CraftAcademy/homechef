9f51ee23ebe7b4bee50a04c3c7c13e5f96b5d621
rmzse <rodrigo@therawdelicious.com>
2017-02-01 12:09:31 +0100

    add 'create dish' route, acceptance test failing, missing input field

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root controller: :landing, action: :index
-  resources :dishes, only: [:index, :show]
+  resources :dishes, only: [:index, :show, :create]
   resources :checkout, only: [:index]
   resources :charges, only: [:create]
   post :add_to_order, controller: :orders, action: :add_to_order
