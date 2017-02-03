cb80b56de153e9bda239b1182a9b9ef4fedf359e
rmzse <rodrigo@therawdelicious.com>
2017-02-03 22:23:52 +0100

    fix form input

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -12,9 +12,8 @@ class DishesController < ApplicationController
 		@user_dish = Dish.create(name: params[:name], description: params[:description],
 											 price: params[:price], portions: params[:portions],
 											 pickup_time: params[:pickup_time], user: current_user.id)
-		User.update(dish: @user_dish.id).where(id: current_user.id)
-		redirect_back(fallback_location: user_dish_path(dish.id))
+		# User.update(dish: @user_dish.id).where(id: current_user.id)
+		redirect_back(fallback_location: new_user_dish_path(current_user))
 		flash[:notice] = "#{@user_dish.name} successfully added to your dishes"
 	end
-
 end
