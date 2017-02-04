9ecc72e9b3e43eb26b988a043d386eb5a2ccc204
rmzse <rodrigo@therawdelicious.com>
2017-02-04 14:29:26 +0100

    add dependency between users and dishes with migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170127074034) do
+ActiveRecord::Schema.define(version: 20170204125845) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 20170127074034) do
     t.integer  "portions"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_dishes_on_user_id", using: :btree
   end
 
   create_table "order_items", force: :cascade do |t|
@@ -62,5 +64,6 @@ ActiveRecord::Schema.define(version: 20170127074034) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "dishes", "users"
   add_foreign_key "orders", "users"
 end
