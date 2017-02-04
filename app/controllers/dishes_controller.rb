e93e9baf5bce0f5783e5f936e83e260eb5c26922
rmzse <rodrigo@therawdelicious.com>
2017-02-04 17:53:26 +0100

    remove redundant flash notice and correct fallback route

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -9,7 +9,7 @@ class DishesController < ApplicationController
 
 	def create
 		@user_dish = Dish.create(name: params[:name], description: params[:description], price: params[:price], portions: params[:portions], ready_time: params[:ready_time], user: current_user)
-		redirect_back(fallback_location: new_user_dish_path(root_path))
+		redirect_back(fallback_location: new_user_dish_path(current_user))
 		flash[:notice] = "#{@user_dish.name} successfully added to your dishes"
 	end
 end
