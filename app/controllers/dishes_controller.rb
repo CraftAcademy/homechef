80fc91e4897df9904825a30d024912dbf0ab580f
rmzse <rodrigo@therawdelicious.com>
2017-02-05 14:24:46 +0100

    add adress functionality to controller

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -1,10 +1,12 @@
 class DishesController < ApplicationController
+	include Geokit::Geocoders
 
 	def new
 	end
 
 	def create
-		@user_dish = Dish.create(name: params[:name], description: params[:description], price: params[:price], portions: params[:portions], ready_time: params[:ready_time], user: current_user)
+		location = GoogleGeocoder.geocode(params[:address])
+		@user_dish = Dish.create(name: params[:name], description: params[:description], price: params[:price], portions: params[:portions], ready_time: params[:ready_time], lat:location.lat, lng:location.lng, user: current_user)
 		redirect_back(fallback_location: new_user_dish_path(current_user))
 		flash[:notice] = "#{@user_dish.portions} portions of #{@user_dish.name} at #{@user_dish.price}$ each successfully added to your dishes"
 	end
